b094e1a2ef86ea7ac5122b228417900dc0952a2d
Kaya Jhuti <itskayaj@gmail.com>
2023-12-06 19:33:02 -0500

    Working prototype with dynamic HTML background

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,64 @@
-import * as NodeWebcam from "node-webcam";
+import express from 'express';
 import * as getColors from "get-image-colors";
+import * as http from 'http';
+import * as NodeWebcam from "node-webcam";
 import * as path from "path";
+import { Server } from 'socket.io';
 
-// Webcam options
-const opts = {
-  width: 1280,
-  height: 720,
-  quality: 100,
-  delay: 0,
-  saveShots: true,
-  output: "jpeg",
-  device: false,
-  callbackReturn: "location",
-  verbose: false
-};
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// Create webcam instance
-const Webcam = NodeWebcam.create(opts);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-// Function to capture image and get average color
-function captureAndAnalyze() {
-  Webcam.capture("current_view", function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      getColors.default(path.resolve(`${__dirname}/../`, "current_view.jpg")).then(colors => {
-        let r = 0, g = 0, b = 0;
-        colors.forEach(color => {
-          r += color._rgb[0];
-          g += color._rgb[1];
-          b += color._rgb[2];
+io.on('connection', (socket) => {
+
+  // Webcam options
+  const opts = {
+    width: 640,
+    height: 480,
+    quality: 50,
+    delay: 0,
+    saveShots: false,
+    output: "jpeg",
+    device: false,
+    callbackReturn: "location",
+    verbose: false
+  };
+
+  // Create webcam instance
+  const Webcam = NodeWebcam.create(opts);
+
+  // Function to capture image and get average color
+  function captureAndAnalyze() {
+    Webcam.capture("current_view", function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        getColors.default(path.resolve(`${__dirname}/../`, "current_view.jpg")).then(colors => {
+          let red = 0, green = 0, blue = 0;
+          colors.forEach(color => {
+            red += color._rgb[0];
+            green += color._rgb[1];
+            blue += color._rgb[2];
+          });
+          red /= colors.length;
+          green /= colors.length;
+          blue /= colors.length;
+          console.log(`Average color is rgb(${red}, ${green}, ${blue})`);
+
+          io.emit('updatedColours', { red, green, blue });
         });
-        r /= colors.length;
-        g /= colors.length;
-        b /= colors.length;
-        console.log(`Average color is rgb(${r}, ${g}, ${b})`);
-      });
-    }
-  });
-}
+      }
+    });
+  }
 
-// Capture and analyze every second
-setInterval(captureAndAnalyze, 1000);
+  setInterval(captureAndAnalyze, 3000);
+
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
